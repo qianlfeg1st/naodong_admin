c8e0c29ce9671d3905af257823520935aff6fb79
钱立峰 <qianlfeg@gmail.com>
2020-08-12 18:17:26 +0800

    fix

---
+++ precache-manifest.9282d7beba26069311a41ef34d911328.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e08aa23e64156ed227570bdeda68a3e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c412c3e4e9c44c1e48b0",
+    "url": "./static/css/2.c5f91ab2.chunk.css"
+  },
+  {
+    "revision": "2fae715e41a679fb8ae2",
+    "url": "./static/css/main.bb2989de.chunk.css"
+  },
+  {
+    "revision": "c412c3e4e9c44c1e48b0",
+    "url": "./static/js/2.186aeba3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.186aeba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fae715e41a679fb8ae2",
+    "url": "./static/js/main.b36eb499.chunk.js"
+  },
+  {
+    "revision": "327a06e6357f5cd919f2",
+    "url": "./static/js/runtime-main.9e38079d.js"
+  },
+  {
+    "revision": "07851b01515d3292f6fae2e6e6ff1baf",
+    "url": "./static/media/404.07851b01.jpg"
+  },
+  {
+    "revision": "c56e1c3e881ecb6eda8adc024b5d6b36",
+    "url": "./static/media/logo.c56e1c3e.png"
+  }
+]);
